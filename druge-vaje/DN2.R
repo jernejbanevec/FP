92444e2651bde56d6cf949fee25f7126f6c5b44b
Jernej <Jernej>
2017-11-15 00:38:05 +0100

    idemoo

--- druge-vaje/DN2.R
+++ druge-vaje/DN2.R
@@ -50,11 +50,22 @@ curve(ppareto1(x, shape = shape, min = min),
 
 
 #d)
+
+#Waldove identitete
+#
+#E(S) = E(E(S|N)) = E(N*E(Y)) = E(Y)*E(N), N stevilo odskodninskih zneskov
+#E(S|N=k) = E(sum|N=k) = E(sum) = sum(E(Yi)) = kE(Y)
+#Var(S) = Var(Y)*E(N) + E(Y)^2*Var(N)
+
 alfa <- shape
 x_m<- min #Predstavlja parameter merila
 
-E_S <- (alfa * x_m) / (alfa - 1)
-Var_S <- Inf #Vrednost je neskončno, saj je alfa v našem primeru < 2
+E_Y <- (alfa * x_m) / (alfa - 1)
+Var_Y <- Inf #Vrednost je neskončno, saj je alfa v našem primeru < 2
+
+E_N <- 15 #Upanje poisonove z lambda = 15
+E_S <- E_Y * E_N
+Var_S <- Inf #Saj je v vsoti tudi Var_Y pomnožena z nečim pozitivnim
 
 ##DRUGA NALOGA
 #a)
@@ -70,9 +81,10 @@ dis_pareto <- diffinv(discretize(ppareto1(x, alfa, x_m),
 #diffinf nam da porazdelitveno funkcijo
 
 #b)
+stopnice <- stepfun(seq(0, 39.5, by = h), 
+                    dis_pareto)
 
-plot(stepfun(seq(0, 39.5, by = h), 
-             dis_pareto),
+plot(stopnice,
      col = "orange",
      do.points = FALSE,
      main = "Paretova porazdelitev",
@@ -83,3 +95,28 @@ curve(ppareto1(x, alfa, x_m),
       from = 0,
       to = 45,
       add = TRUE)
+
+#c)
+
+porazdelitvena <- aggregateDist(method = "recursive", 
+                                model.freq = "poisson", 
+                                model.sev = diff(dis_pareto), #diff nam da višino skoka (boljše predstavlja verjetnost)
+                                lambda = 15,
+                                p0 = NULL,
+                                x.scale = h)
+plot(porazdelitvena) #Nariše graf - NAROBE!!!
+
+#d)
+
+#tu je S diskretna slučajna spremenljivka
+vrednosti1 <- knots(stopnice)
+vrednosti <- 0.5 * (2 * vrednosti + 1)
+verjetnosti <- diff(dis_pareto)
+
+upanje <- (vrednosti %*% verjetnosti) * 15 #to je skalarni produkt
+razdalja <- vrednosti - E_Y #to je uredu zaradi krožnega dopolnjevanja
+varianca <- (razdalja * razdalja) %*% verjetnosti
+
+#e)
+
+odst_95 <- VaR(porazdelitvena)[2]
